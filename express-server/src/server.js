98a34c947e5456684f7d0b0671a009608a17d119
Anastasia Radchuk <nastasiyaradchuk@gmail.com>
2023-07-01 01:09:14 -0400

    added sample endpoints

--- express-server/src/server.js
+++ express-server/src/server.js
@@ -1,9 +1,21 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
-app.get('/hello', (req, res) => {
+app.get('/', (req, res) => {
     res.send("Hey there!");
+});
+
+app.post('/hello', (req, res) => {
+    console.log(req.body);
+    res.send(`Hey ${req.body.name}!`);
+});
+
+app.get('/hello/:name', (req, res) => {
+    const { name } = req.params;
+    console.log(name);
+    res.send(`Hello ${name}!`);
 });
 
 app.listen(8000, () => {
